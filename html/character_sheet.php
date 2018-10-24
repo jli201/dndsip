d5a63bc635cefc703b1e2611e3b26eb2da7338d3
Jensen <jli201@ucsc.edu>
2018-10-24 01:51:24 +0000

    Updated character sheet to load css correctly.

--- html/character_sheet.php
+++ html/character_sheet.php
@@ -4,9 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
   <title>Character Sheet</title>
-  <style>
-	  <link rel="stylesheet" href="character_sheet.css">	  
-  </style>
+  <link rel="stylesheet" href="character_sheet.css">
 </head>
 <body>
 	<div id="namePlate">
@@ -15,9 +13,6 @@
 	<div class="column">
 		<div id="stats">
 			<p>This is the color that I would like to have the internal divs use.</p>
-		</div>
-		<div id="passiveWisdom">
-			<p>This is the last color in the color pallete that I had generated. I think it is neat too.</p>
 		</div>
 	</div>
 	<div class="column">
