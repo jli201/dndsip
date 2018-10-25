210569d8235ad2d0bfcac8b213a0338125f2bc5d
awong35 <allanwong98@gmail.com>
2018-10-25 01:05:37 -0700

    worked on nameplate

--- html/character_sheet.php
+++ html/character_sheet.php
@@ -4,13 +4,27 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
   <title>Character Sheet</title>
-  <style>
-	  <link rel="stylesheet" href="character_sheet.css">	  
-  </style>
+  <link rel="stylesheet" href="character_sheet.css">	  
 </head>
 <body>
 	<div id="namePlate">
-		<p>Content here</p>
+		<div id="innerbox">
+		<!--	<input style="height: 80%;" type="text" name="charname" placeholder="Character Name"> -->
+		<form id="nameform">
+			<label style="font-weight: bold; font-size: 16px;">Character Name: </label>
+			<input style="font-weight: bold;" type="text" placeholder="Character Name">
+			<label style="font-size: 13px;"> Class & Level: </label>
+			<input type="text" placeholder="Class & Level">
+			<label style="font-size: 13px;"> Player Name: </label>
+			<input type="text" placeholder="Player Name">
+			<label style="font-size: 13px;"> Race: </label>
+			<input type="text" placeholder="Race">
+			<label style="font-size: 13px;"> Alignment: </label>
+			<input type="text" placeholder="Alignment">
+			<label style="font-size: 13px;"> Experience Points: </label>
+			<input type="text" placeholder="Experience Points">
+		</form>
+		</div>
 	</div>
 	<div class="column">
 		<div id="stats">
