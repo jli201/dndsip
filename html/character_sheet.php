4a52092b7299710d7080f92d578a79a83e1418d7
Allan <awong35@ucsc.edu>
2018-10-26 19:42:28 -0700

    changed the layout for name plate a bit

--- html/character_sheet.php
+++ html/character_sheet.php
@@ -13,17 +13,17 @@
 		<div id="innerbox">
 		<!--	<input style="height: 80%;" type="text" name="charname" placeholder="Character Name"> -->
 		<form id="nameform">
-			<label style="font-weight: bold; font-size: 16px;">Character Name: </label>
-			<input style="font-weight: bold;" type="text" placeholder="Character Name">
-			<label style="font-size: 13px;"> Class & Level: </label>
-			<input type="text" placeholder="Class & Level">
-			<label style="font-size: 13px;"> Player Name: </label>
+			<label style="font-size: 14px;"> Player Name: </label>
 			<input type="text" placeholder="Player Name">
-			<label style="font-size: 13px;"> Race: </label>
+			<label style="font-size: 14px;">Character Name: </label>
+			<input type="text" placeholder="Character Name">
+			<label style="font-size: 14px;"> Class & Level: </label>
+			<input type="text" placeholder="Class & Level">
+			<label style="font-size: 14px;"> Race: </label>
 			<input type="text" placeholder="Race">
-			<label style="font-size: 13px;"> Alignment: </label>
+			<label style="font-size: 14px;"> Alignment: </label>
 			<input type="text" placeholder="Alignment">
-			<label style="font-size: 13px;"> Experience Points: </label>
+			<label style="font-size: 14px;"> Experience Points: </label>
 			<input type="text" placeholder="Experience Points">
 		</form>
 		</div>
