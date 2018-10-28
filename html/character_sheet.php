28049ffe911e8df859a9dbed055dacad53b7b65a
Allan <awong35@ucsc.edu>
2018-10-27 21:08:01 -0700

    worked on inspiration/proficiency/stats/saving throws

--- html/character_sheet.php
+++ html/character_sheet.php
@@ -12,25 +12,79 @@
 	<div id="namePlate">
 		<div id="innerbox">
 		<!--	<input style="height: 80%;" type="text" name="charname" placeholder="Character Name"> -->
-		<form id="nameform">
-			<label style="font-size: 14px;"> Player Name: </label>
-			<input type="text" placeholder="Player Name">
-			<label style="font-size: 14px;">Character Name: </label>
-			<input type="text" placeholder="Character Name">
-			<label style="font-size: 14px;"> Class & Level: </label>
-			<input type="text" placeholder="Class & Level">
-			<label style="font-size: 14px;"> Race: </label>
-			<input type="text" placeholder="Race">
-			<label style="font-size: 14px;"> Alignment: </label>
-			<input type="text" placeholder="Alignment">
-			<label style="font-size: 14px;"> Experience Points: </label>
-			<input type="text" placeholder="Experience Points">
-		</form>
+			<form id="nameform">
+				<label style="font-size: 14px;"> Player Name: </label>
+				<input type="text" placeholder="Player Name">
+				<label style="font-size: 14px;">Character Name: </label>
+				<input type="text" placeholder="Character Name">
+				<label style="font-size: 14px;"> Class & Level: </label>
+				<input type="text" placeholder="Class & Level">
+				<label style="font-size: 14px;"> Race: </label>
+				<input type="text" placeholder="Race">
+				<label style="font-size: 14px;"> Alignment: </label>
+				<input type="text" placeholder="Alignment">
+				<label style="font-size: 14px;"> Experience Points: </label>
+				<input type="text" placeholder="Experience Points">
+			</form>
 		</div>
 	</div>
 	<div class="column">
 		<div id="stats">
-			<p>This is the color that I would like to have the internal divs use.</p>
+		
+			<!-- inspiration and proficiency bonus -->
+			<div id="insp-prof"> 
+				<input type="number" style="margin-bottom: 2px;">
+				<label style="font-size: 20px;">Inspiration</label>
+			</div>
+			<div id="insp-prof"> 
+				<input type="number">
+				<label style="font-size: 20px;">Proficiency Bonus</label>
+			</div>
+			
+			<!-- stats boxes -->
+			<div id="statsboxLeftCol">
+				<h2> Strength </h2>
+				<input type="number">
+				<input type="number" style="width: 50px; margin-top: 2px;">
+			</div>
+			<div id="statsboxLeftCol">
+				<h2> Dexterity </h2>
+				<input type="number">
+				<input type="number" style="width: 50px; margin-top: 2px;">
+			</div>
+			<div id="statsboxLeftCol">
+				<h2> Constitution </h2>
+				<input type="number">
+				<input type="number" style="width: 50px; margin-top: 2px;">
+			</div>
+			<div id="statsboxLeftCol">
+				<h2> Intelligence </h2>
+				<input type="number">
+				<input type="number" style="width: 50px; margin-top: 2px;">
+			</div>
+			<div id="statsboxLeftCol">
+				<h2> Wisdom </h2>
+				<input type="number">
+				<input type="number" style="width: 50px; margin-top: 2px;">
+			</div>
+			<div id="statsboxLeftCol">
+				<h2> Charisma </h2>
+				<input type="number">
+				<input type="number" style="width: 50px; margin-top: 2px;">
+			</div>
+			
+			<!-- saving throws -->
+			<div id="savingThrows">
+				<form id="ST-form">
+					<input id="ST-form-checkbox" type="checkbox"> <input type="number" id="ST-form-spacing" style="width: 20px;"> Strength <br>
+					<input id="ST-form-checkbox" type="checkbox"> <input type="number" id="ST-form-spacing" style="width: 20px;"> Dexterity <br>
+					<input id="ST-form-checkbox" type="checkbox"> <input type="number" id="ST-form-spacing" style="width: 20px;"> Constitution <br>
+					<input id="ST-form-checkbox" type="checkbox"> <input type="number" id="ST-form-spacing" style="width: 20px;"> Intelligence <br>
+					<input id="ST-form-checkbox" type="checkbox"> <input type="number" id="ST-form-spacing" style="width: 20px;"> Wisdom <br>
+					<input id="ST-form-checkbox" type="checkbox"> <input type="number" id="ST-form-spacing" style="width: 20px;"> Charisma <br>
+				</form>
+			</div>
+
 		</div>
 		<div id="passiveWisdom">
 			<p>This is the last color in the color pallete that I had generated. I think it is neat too.</p>
