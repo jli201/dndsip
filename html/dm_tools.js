c5f7801fb15f3883052ec855f7833b744498fca9
visingh19 <visingh@ucsc.edu>
2018-11-28 01:30:02 -0800

    CMPS115 S4: Added basic skeleton for dm_tools page. All linked sheets work.

---
+++ html/dm_tools.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+	console.log("Welcome to DM Tools!");
+});
---
+++ html/dm_tools.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>DNDSIP: DM TOOLS</title>
+
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="dm_tools.css">
+
+		<!-- JQUERY & JS -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="dm_tools.js"></script>
+
+	</head>
+	<body>
+		<p>Hi.</p>
+
+
+	</body>
+</html>
