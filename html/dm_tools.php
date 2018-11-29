b84528cd2f201c95ba9c42f208debbf0fd0fa37a
visingh19 <visingh@ucsc.edu>
2018-11-29 01:28:15 -0800

    basic box sizing.

--- html/dm_tools.php
+++ html/dm_tools.php
@@ -8,11 +8,18 @@
 		<!-- JQUERY & JS -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="dm_tools.js"></script>
+		<script>
+	  	$(document).ready(function(){
+		    $('#diceroller').load("diceroller.html");
+		});
+  </script>
 
 	</head>
 	<body>
-		<p>Hi.</p>
-
+		<div id="diceroller"></div>
+		<div id="pagetitle">DM TOOLS</div>
+		<div id="initTrackerWrapper" class="column">a</div>
+		<div id="notesWrapper" class="column">a</div>
 
 	</body>
 </html>
