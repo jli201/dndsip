ef0e068d612b0bdfdead64bbdfc6c280207201ee
visingh19 <visingh@ucsc.edu>
2018-11-29 04:32:03 -0800

    bug fixes to constructor and enqueue operations

--- html/dm_tools.js
+++ html/dm_tools.js
@@ -8,9 +8,10 @@ $(document).ready(function() {
 //https://www.geeksforgeeks.org/implementation-priority-queue-javascript/
 //Created with reference to the above. But seriously anyone who passed 101 can make a pqueue.
 class TurnElement {
-	constructor(name, roll) {
+	constructor(cname, roll, ally) {
 		this.cname = cname; //character name
-		this.roll = roll; //character roll
+		this.roll = Number(roll); //character roll - use Number() to make comparisons num based, not string alpha based
+		this.ally = ally; //friend or foe?
 	}
 }
 
@@ -26,16 +27,19 @@ class TurnOrder {
 	// ENQUEUE, DEQUEUE, FRONT, ISEMPTY, DUMPQUEUE
 
 	// ENQUEUE - rolls are the priority value
-	enqueue(cname, roll) {
-		var turn = new TurnElement(cname, roll);
+	enqueue(cname, roll, ally) {
+		var turn = new TurnElement(cname, roll, ally);
 
 		//simple insertion sort
 		var i;
 		for (i = 0; i < this.turns.length; i++ ) {
 			if (this.turns[i].roll < turn.roll) {
 				this.turns.splice(i, 0, turn); //index, # items to remove, any # of args to add 
+				return;
 			}
 		}
+
+		this.turns.push(turn); //if turn.roll is ALWAYS smaller than all others, add to back
 
 	}
 
@@ -49,6 +53,7 @@ class TurnOrder {
 
 	//delete the element that clicked its delete button
 	//https://www.w3schools.com/jsref/jsref_splice.asp - splice.
+	// ally status doesn't matter
 	findDelete(cname, roll) {
 		var i;
 		for (i = 0; i < this.turns.length; i++ ) {
@@ -92,7 +97,18 @@ class TurnOrder {
 // Functions for interaction with queue.
 
 function addTurn() {
-	// var cname = 
+	var cname = $('#initNewTurnName').val();
+	var roll = $('#initNewTurnRoll').val();
+	var ally = $('#initNewTurnEnemy').prop("checked");
+
+	// console.log(cname + " " + roll + " " + ally);
+	if (cname.length == 0 || roll.length == 0 ) { //values not entered
+		console.log("Some values not inputted.");
+		return;
+	}
+
+	turnList.enqueue(cname, roll, ally);
+	console.log(turnList.dumpQueue());
 }
 
 
